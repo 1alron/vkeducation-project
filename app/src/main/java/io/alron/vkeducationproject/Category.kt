b8ef2019be3d404e5abc35b868f929d65e94c979
1alron <roma.niko393@gmail.com>
2026-03-10 10:46:33 +0300

    app-card-and-list Добавлена карточка приложения

---
+++ app/src/main/java/io/alron/vkeducationproject/Category.kt
@@ -0,0 +1,5 @@
+package io.alron.vkeducationproject
+
+enum class Category {
+    APP, GAME
+}
---
+++ app/src/main/java/io/alron/vkeducationproject/Developer.kt
@@ -0,0 +1,52 @@
+package io.alron.vkeducationproject
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import io.alron.vkeducationproject.ui.theme.VKEducationProjectTheme
+
+@Composable
+fun Developer(
+    name: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier.clickable(onClick = onClick),
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+        Column {
+            Text(text = name)
+            Text(text = stringResource(R.string.app_details_developer))
+        }
+        IconButton(onClick = onClick) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Default.KeyboardArrowRight,
+                contentDescription = null,
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun Preview() {
+    VKEducationProjectTheme {
+        Developer(
+            name = "VK Play",
+            onClick = {},
+            modifier = Modifier.fillMaxWidth(),
+        )
+    }
+}
